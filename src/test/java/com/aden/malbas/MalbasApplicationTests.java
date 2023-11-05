b481b499ba3cf39a422cc2a26394513e8ae64e23
omarkhaled646 <63152184+omarkhaled646@users.noreply.github.com>
2023-11-05 12:35:25 +0200

    initial commit

---
+++ src/test/java/com/aden/malbas/MalbasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aden.malbas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MalbasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
